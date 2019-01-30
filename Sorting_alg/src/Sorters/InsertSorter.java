508e81ba99647714361d4360df65359158994251
palamalama <ericgemmell@gmail.com>
2019-01-30 17:56:31 +0000

    Added Quick Sorter with Cuttoff

--- Sorting_alg/src/Sorters/InsertSorter.java
+++ Sorting_alg/src/Sorters/InsertSorter.java
@@ -17,6 +17,18 @@ public class InsertSorter extends Sorter {
 			Sorted[i+1] = valueAtJ;
 		}
 	}
+	public void SortSubArray(int[] A,int p, int r) {
+		for (int j = p+1; j < r+1;j++) {
+			int valueAtJ = A[j];
+			
+			int i = j-1;
+			while (i >= 0 && A[i] > valueAtJ) {
+				A[i+1] = A[i];
+				i--;
+			}
+			A[i+1] = valueAtJ;
+		}
+	}
 	public static void main(String[] Args) {
 		int[] a = new int[]{8,23,8,32,90,3,6,1,0,45,2,67,2,42};
 		Sorter sorter = new InsertSorter();
--- Sorting_alg/src/Sorters/QuickCutOffSorter.java
+++ Sorting_alg/src/Sorters/QuickCutOffSorter.java
@@ -1,11 +1,51 @@
 package Sorters;
 
-public class QuickCutOffSorter extends Sorter {
+import java.util.Arrays;
 
+public class QuickCutOffSorter extends Sorter {
+	InsertSorter cutOffSorter;
+	
+	public QuickCutOffSorter() {
+		this.cutOffSorter = new InsertSorter();
+	}
+	
 	@Override
 	public void SortIntArray(int[] toSort) {
-		// TODO Auto-generated method stub
-
+		QuickSort(toSort,0,toSort.length-1);
+	}
+	public void QuickSort(int[] A, int p, int r) {
+		if(p<r) {
+			if(r-p<10) {
+				cutOffSorter.SortSubArray(A,p,r);
+			}
+			int q = Partition(A, p,r);
+			QuickSort(A,p,q-1);
+			QuickSort(A,q+1,r);
+		}
+	}
+	public int Partition(int[] A,int p, int r) {
+		int pivot = A[r];
+		int i = p-1;
+		for(int j = p; j < r; j++) {
+			if(A[j]<=pivot) {
+				i++;
+				int temp = A[j];
+				A[j] = A[i];
+				A[i] = temp;
+			}
+		}
+		i++;
+		int temp = A[r];
+		A[r] = A[i];
+		A[i] = temp;
+		return i;
+	}
+	
+	public static void main(String[] Args) {
+		int[] a = new int[]{8,23,8,32,90,3,6,1,0,45,2,67,2,42};
+		Sorter sorter = new QuickCutOffSorter();
+		sorter.SortIntArray(a);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
